8fcba1d7ed4e4d49c3c5db8a6c8db421d6bdffbe
Andy <becker.andrew2@gmail.com>
2016-03-23 10:36:13 -0400

    Changed database user

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
+	sequelize = new Sequelize('caroli30_recipes', 'caroli30_noderec', 'U8Er7g55yILmbtfkxH', {
 		dialect: 'postgres'
 	});
 } else {
