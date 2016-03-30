5e654fceca5568ebbd35c4ccf12032f3660b2bca
Andy <becker.andrew2@gmail.com>
2016-03-30 13:05:13 -0400

    Uncommented routes to create user account

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var authController = require('../controllers/authController');
 
 router.get('/', authController.loginForm);
-// router.get('/users/signup', authController.signup);
-// router.post('/users', authController.createAccount);
+router.get('/users/signup', authController.signup);
+router.post('/users', authController.createAccount);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
