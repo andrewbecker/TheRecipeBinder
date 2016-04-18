efd9ba932c5a383b8fb7f5b502fb1af5439f0eb1
Andy <becker.andrew2@gmail.com>
2016-04-18 11:22:38 -0400

    Removed user signup routes

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var authController = require('../controllers/authController');
 
 router.get('/', authController.loginForm);
-router.get('/signup', authController.signup);
-router.post('/', authController.createAccount);
+//router.get('/signup', authController.signup);
+//router.post('/', authController.createAccount);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
