6e3a580ef9e98113bc0b3e561103bc086f2b2c0a
Andy <becker.andrew2@gmail.com>
2016-05-02 10:50:51 -0400

    Added setInterval to change recipe card size when browser window is resized

--- public/javascripts/recipe-card-resize.js
+++ public/javascripts/recipe-card-resize.js
@@ -2,19 +2,49 @@ $(function(){
 
 	var heightArray = [];
 	var maxHeight = 0;
+	var windowWidth = $('.main-area').outerWidth();
+
 	$('.recipe-card').each(function (i, obj) {
-		heightArray.push($(this).height());
-		// console.log($('.main-area').width());
-		// if ($('.main-area').width() === 1140) {
-		// 	var newWidth = $(this).width() - 14;
-		// 	$(this).width(newWidth);
-		// }
+		var imgHeight, titleHeight, descriptionHeight, totalHeight;
+		imgHeight = $(this).find('img').innerHeight();
+		titleHeight = $(this).find('h3').outerHeight();
+		descriptionHeight = $(this).find('p').innerHeight();
+		totalHeight = (imgHeight + titleHeight + descriptionHeight + 40);
+
+		heightArray.push(totalHeight);
 	});
-	console.log("Array: " + heightArray);
+	
 	var maxHeight = (Math.max(...heightArray));
 
 	$('.recipe-card').each(function () {
 		$(this).height(maxHeight);
 	});
+
+	var checkWidth = setInterval(function(){
+		var newWindowWidth = $('.main-area').outerWidth();
+		if (newWindowWidth !== windowWidth) {
+			windowWidth = newWindowWidth;
+			heightArray = [];
+
+			$('.recipe-card').each(function (i, obj) {
+				var imgHeight, titleHeight, descriptionHeight, totalHeight;
+				imgHeight = $(this).find('img').innerHeight();
+				titleHeight = $(this).find('h3').outerHeight();
+				descriptionHeight = $(this).find('p').innerHeight();
+				totalHeight = (imgHeight + titleHeight + descriptionHeight + 40);
+
+
+
+				heightArray.push(totalHeight);
+
+			});
+
+			var maxHeight = (Math.max(...heightArray));
+
+			$('.recipe-card').each(function () {
+				$(this).height(maxHeight);
+			});
+		};
+	}, 250);
 
 });
